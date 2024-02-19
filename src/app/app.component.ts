d0b095d5e53464cb68b2453b69f3b232b55e1a0d
aziz <aziz.akari001@gmail.com>
2024-02-19 11:22:44 +0100

    mywebsitev01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, RouterLink, RouterLinkActive ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PresentationComponent } from './presentation/presentation.component';
+import { ContactComponent } from './contact/contact.component';
+import { GallerieComponent } from './gallerie/gallerie.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'presentation', component: PresentationComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'gallerie', component: GallerieComponent},
+
+];
