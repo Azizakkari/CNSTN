687ed5bc5e1d0d53dd8c078886de2465de5ac9a4
aziz <aziz.akari001@gmail.com>
2024-03-06 11:21:13 +0100

    cnstn new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,17 @@ import { PresentationComponent } from './presentation/presentation.component';
 import { ContactComponent } from './contact/contact.component';
 import { AcceuilcnstnComponent } from './acceuilcnstn/acceuilcnstn.component';
 import  jsondata from   '../data/fichier.json';
+import jsondataproc from '../data/procedures.json'
 import { Header2Component } from './header2/header2.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, FooterComponent, RouterLink, RouterLinkActive, Header2Component ],
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cnstnnewsite';
   mesfichier: any[]=jsondata.files;
+  mesproc:any[]=jsondataproc.procedures;
 }
